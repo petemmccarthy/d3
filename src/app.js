b28a8c3b37366e30fb0fec269393798d89a37d69
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2016-12-07 11:05:43 +0000

    rotated bars to columns

--- src/app.js
+++ src/app.js
@@ -4,33 +4,35 @@
 // var div = body.append("div");
 // div.html("New data");
 
-var width = 420,
-    barHeight = 20;
+var width = 960,
+    height = 500;
 
-var x = d3.scaleLinear()
-  .range([0, width]);
+var y = d3.scaleLinear()
+  .range([height, 0]);
 
 var chart = d3.select('.chart')
   .attr('width', width)
+  .attr('height', height);
 
 d3.tsv("data.tsv", type, function(error, data) {
-  x.domain([0, d3.max(data, function(d) { return d.value; })]);
+  y.domain([0, d3.max(data, function(d) { return d.value; })]);
 
-  chart.attr("height", barHeight * data.length);
-  console.log(data);
+  var barWidth = width / data.length;
+
   var bar = chart.selectAll("g")
       .data(data)
     .enter().append("g")
-      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+      .attr("transform", function(d, i) { return "translate(" + i * barWidth + ")"; });
 
   bar.append("rect")
-      .attr("width", function(d) { return x(d.value); })
-      .attr("height", barHeight - 1);
+      .attr("y", function(d) { return y(d.value); })
+      .attr("height", function(d) { return height - y(d.value); })
+      .attr("width", barWidth - 1);
 
   bar.append("text")
-      .attr("x", function(d) { return x(d.value) - 3; })
-      .attr("y", barHeight / 2)
-      .attr("dy", ".35em")
+      .attr("x", barWidth / 2)
+      .attr("y", function(d) { return y(d.value) + 3; })
+      .attr("dy", ".75em")
       .text(function(d) { return d.value; });
 });
 
